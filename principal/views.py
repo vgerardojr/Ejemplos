5d8264d00875f4811f87aa812f331621d401221b
Gerardo Vasquez <vasquezgerardojr@gmail.com>
2013-05-02 22:45:40 -0430

    Modica la cosulta en la vista

--- principal/views.py
+++ principal/views.py
@@ -3,5 +3,5 @@ from principal.models import Receta
 from django.shortcuts import render_to_response
 
 def lista_recetas(request):
-	recetas = Receta.objects.exclude(ingredientes__startswith='d')
+	recetas = Receta.objects.all()
 	return render_to_response('lista_recetas.html',{'lista':recetas})
